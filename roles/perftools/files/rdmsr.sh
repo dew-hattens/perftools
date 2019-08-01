17ca11cf15c94f4e600f0db3699f85dd79ded339
Martin Dew-Hattens <martin@dew-hattens.com>
2019-08-01 20:15:13 +0200

    new

---
+++ roles/perftools/files/rdmsr.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+yum install -y help2man
+
+#AUTOMAKE
+rm -rf /tmp/automake
+mkdir -p /tmp/automake
+cd /tmp/autotmake
+curl -v ftp://ftp.gnu.org/gnu/automake/automake-1.16.1.tar.gz --output automake.tar.gz
+gunzip automake.tar.gz
+tar -xvf automake.tar
+rm -f *tar
+cd automake*
+./configure
+make
+make install
+cd ..
+rm -rf /tmp/automake
+
+
+#yum install -y libtool
+
+#RDMSR
+export PATH=$PATH:/usr/local/bin
+rm -rf /tmp/rdmsr
+mkdir /tmp/rdmsr
+cd /tmp/rdmsr
+git clone https://github.com/01org/msr-tools.git
+cd msr-tools
+./autogen.sh
