998726bf0e69c0c806ae3ee6d48ab885f3b6d32e
Martin Dew-Hattens <martin@dew-hattens.com>
2019-08-01 20:24:34 +0200

    up

--- roles/perftools/files/rdmsr.sh
+++ roles/perftools/files/rdmsr.sh
@@ -5,7 +5,7 @@ yum install -y help2man
 #AUTOMAKE
 rm -rf /tmp/automake
 mkdir -p /tmp/automake
-cd /tmp/autotmake
+cd /tmp/automake
 curl -v ftp://ftp.gnu.org/gnu/automake/automake-1.16.1.tar.gz --output automake.tar.gz
 gunzip automake.tar.gz
 tar -xvf automake.tar
